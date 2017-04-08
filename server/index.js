f3d34b822e16e8e39669341b2159370ae455ad8f
Eric Hebert <ayebear@gmail.com>
2017-04-08 14:31:27 -0400

    Setup working graphql queries

--- server/index.js
+++ server/index.js
@@ -1,10 +1,55 @@
+var { graphql, buildSchema } = require('graphql');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+	type Tutorial {
+		title: String
+		author: String
+		content: String
+	}
+	type Query {
+		hello(name: String): String
+		list: [Tutorial]
+	}
+`);
+
+class Tutorial {
+	constructor(title, author, content) {
+		this.title = title
+		this.author = author
+		this.content = content
+	}
+}
+
+// The root provides a resolver function for each API endpoint
+var root = {
+	hello: (args) => {
+		return `Hello ${args.name}!`;
+	},
+	list: () => {
+		return [
+			new Tutorial("JavaScript ES6", "Eric", "Some JS content here"),
+			new Tutorial("C++17", "Kevin", "Some C++ content here")
+		]
+	}
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
+
+
+
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-	schema: MyGraphQLSchema,
+	schema: schema,
+	rootValue: root,
 	graphiql: true
 }));
 
