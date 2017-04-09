543054b72d395d5de348b48eebb4490b0bbd9d20
Eric Hebert <ayebear@gmail.com>
2017-04-08 20:26:29 -0400

    Added monk to communicate with mongodb GraphQL queries now get data from mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
-var { graphql, buildSchema } = require('graphql');
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+var { graphql, buildSchema, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = require('graphql')
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const db = require('monk')('localhost/youtorials')
+const tutorials = db.get('tutorials')
 
+/*
+	List items in a group, which can contain other groups and tutorials
+	View tutorials
+	Save tutorials
+	Delete tutorials
+*/
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -13,44 +21,48 @@ var schema = buildSchema(`
 	type Query {
 		hello(name: String): String
 		list: [Tutorial]
+		get(author: String): Tutorial
 	}
-`);
-
-class Tutorial {
-	constructor(title, author, content) {
-		this.title = title
-		this.author = author
-		this.content = content
-	}
-}
+`)
 
 // The root provides a resolver function for each API endpoint
 var root = {
 	hello: (args) => {
-		return `Hello ${args.name}!`;
+		return `Hello ${args.name}!`
 	},
 	list: () => {
-		return [
-			new Tutorial("JavaScript ES6", "Eric", "Some JS content here"),
-			new Tutorial("C++17", "Kevin", "Some C++ content here")
-		]
+
+	},
+	get: (args) => {
+		return tutorials.findOne({ author: args.author }).then(result => {
+			return result
+		})
 	}
-};
+}
 
 // Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+graphql(schema, '{ get(author: "Kevin") {author, title, content} }', root).then((response) => {
+	console.log(response)
+})
 
 
 
+// Database testing
+// tutorials.insert(new Tutorial("JavaScript ES6", "Eric", "Some JS content here"))
+// tutorials.insert(new Tutorial("C++17", "Kevin", "Some C++ content here"))
 
-const app = express();
+// tutorials.findOne({ author: 'Kevin' }).then(result => {
+// 	console.log(result)
+// })
+
+
+
+const app = express()
 
 app.use('/graphql', graphqlHTTP({
 	schema: schema,
 	rootValue: root,
-	graphiql: true
-}));
+	graphiql: false
+}))
 
-app.listen(4000);
+app.listen(4000)
